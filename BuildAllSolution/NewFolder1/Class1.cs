1b4944d3aa389627a74864e8b04a2c08d27a9210
Hron <mealiette@seznam.cz>
2016-09-16 13:18:55 +0200

    just fun

---
+++ BuildAllSolution/NewFolder1/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BuildAllSolution.NewFolder1
+{
+	class Class1
+	{
+		public void Foo(bool endless)
+		{
+			int i = 1;
+			while(endless)
+			{
+				i++;
+			}
+		}
+	}
+}
